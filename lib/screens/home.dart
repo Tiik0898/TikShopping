4d5af59420c94d6842e0b8f1d6a83b4586269b66
tik <sukanlaya.tik@vinpoint.co.th>
2019-07-24 14:31:56 +0700

    update

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -7,23 +7,27 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
 //  Explicit
-double myWidth =200.0;
+  double myWidth = 200.0;
 // Method
   Widget showAppName() {
-    return Text(
-      'Tik Shopping',
-      style: TextStyle(
-        fontSize: 30.0,
-        color: Colors.red,
-        fontWeight: FontWeight.bold,
+    return Container(
+      margin: EdgeInsets.only(bottom: 8.0),
+      child: Text(
+        'Tik Shopping',
+        style: TextStyle(
+          fontSize: 30.0,
+          color: Colors.blue[500],
+          fontWeight: FontWeight.bold,
+          fontFamily: 'Fascinate',
+        ),
       ),
     );
   }
 
   Widget signInButton() {
-    return Container(width:200.0,
+    return Container(
       child: FlatButton(
-        color: Colors.green[700],
+        color: Colors.brown[700],
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(30.0),
         ),
@@ -37,9 +41,42 @@ double myWidth =200.0;
   }
 
   Widget signUpButton() {
-    return RaisedButton(
-      child: Text('Sign Up'),
-      onPressed: () {},
+    return Container(
+      child: OutlineButton(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30.0),
+        ),
+        borderSide: BorderSide(
+          color: Colors.blue[700],
+        ),
+        child: Text('Sign Up'),
+        onPressed: () {},
+      ),
+    );
+  }
+
+  Widget showButton() {
+    return Container(
+      width: 250.0,
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: signInButton(),
+          ),
+          Expanded(
+            child: signUpButton(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget showLogo() {
+    return Container(
+      margin: EdgeInsets.only(bottom: 30.0),
+      width: 180.0,
+      height: 180.0,
+      child: Image.asset('images/logo.png'),
     );
   }
 
@@ -47,12 +84,18 @@ double myWidth =200.0;
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        decoration: BoxDecoration(
+          gradient: RadialGradient(
+            colors: [Colors.white, Colors.brown[700]],
+            radius: 1.0,
+          ),
+        ),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            showLogo(),
             showAppName(),
-            signInButton(),
-            signUpButton(),
+            showButton(),
           ],
         ),
         alignment: Alignment.center,
