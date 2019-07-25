4c82d566356c46bed953aa55c51a1cf5c36f442b
tik <sukanlaya.tik@vinpoint.co.th>
2019-07-25 14:38:43 +0700

    Register Success

--- lib/main.dart
+++ lib/main.dart
@@ -8,8 +8,8 @@ main() {
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {//SystemChrome.setPreferredOrientations(orientations)
-    return MaterialApp(
+  Widget build(BuildContext context) {SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp,DeviceOrientation.portraitDown]);
+    return MaterialApp(debugShowCheckedModeBanner: false,
       home:Home(),
     );
   }
--- lib/screens/sign_up.dart
+++ lib/screens/sign_up.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:tik_shopping/screens/my_service.dart';
 
 class SignUp extends StatefulWidget {
   @override
@@ -8,6 +10,9 @@ class SignUp extends StatefulWidget {
 class _SignUpState extends State<SignUp> {
   // Explicit
   double myPadding = 20.0;
+  String nameString, emailString, passwordString;
+  final formKey = GlobalKey<FormState>();
+  FirebaseAuth firebaseAuth = FirebaseAuth.instance;
   // Method
   Widget showHead() {
     return Container(
@@ -34,6 +39,14 @@ class _SignUpState extends State<SignUp> {
               Icons.face,
               size: 40.0,
             )),
+        validator: (String value) {
+          if (value.isEmpty) {
+            return 'Please Fill Name in Blank';
+          }
+        },
+        onSaved: (String value) {
+          nameString = value;
+        },
       ),
     );
   }
@@ -44,11 +57,20 @@ class _SignUpState extends State<SignUp> {
       child: TextFormField(
         keyboardType: TextInputType.emailAddress,
         decoration: InputDecoration(
-            labelText: 'Email :',
-            icon: Icon(
-              Icons.email,
-              size: 40.0,
-            )),
+          labelText: 'Email :',
+          icon: Icon(
+            Icons.email,
+            size: 40.0,
+          ),
+        ),
+        validator: (String value) {
+          if (!((value.contains('@')) && (value.contains('.')))) {
+            return 'Please Type Email Format';
+          }
+        },
+        onSaved: (String value) {
+          emailString = value;
+        },
       ),
     );
   }
@@ -58,11 +80,20 @@ class _SignUpState extends State<SignUp> {
       padding: EdgeInsets.only(left: myPadding, right: myPadding),
       child: TextFormField(
         decoration: InputDecoration(
-            labelText: 'Password :',
-            icon: Icon(
-              Icons.vpn_key,
-              size: 40.0,
-            )),
+          labelText: 'Password :',
+          icon: Icon(
+            Icons.vpn_key,
+            size: 40.0,
+          ),
+        ),
+        validator: (String value) {
+          if (value.length < 6) {
+            return 'More 6 Charactor';
+          }
+        },
+        onSaved: (String value) {
+          passwordString = value;
+        },
       ),
     );
   }
@@ -70,8 +101,69 @@ class _SignUpState extends State<SignUp> {
   Widget uploadButton() {
     return IconButton(
       icon: Icon(Icons.cloud_upload),
-      onPressed: () {},
+      onPressed: () {
+        print('You Click Upload');
+        if (formKey.currentState.validate()) {
+          formKey.currentState.save();
+          print(
+              'name =$nameString,email = $emailString,password =$passwordString');
+          registerUser();
+        }
+      },
     );
+  }
+
+  Future<void> registerUser() async {
+    await firebaseAuth
+        .createUserWithEmailAndPassword(
+            email: emailString, password: passwordString)
+        .then((response) {
+      print('Register Success');
+      setupDisplayName();
+    }).catchError((response) {
+      print('Error =${response.toString()}');
+      String titleString = response.code;
+      String messageString = response.message;
+      myAlert(titleString, messageString);
+    });
+  }
+
+  Future<void> setupDisplayName() async {
+    FirebaseUser firebaseUser = await firebaseAuth.currentUser();
+    UserUpdateInfo userUpdateInfo = UserUpdateInfo();
+    userUpdateInfo.displayName = nameString;
+    firebaseUser.updateProfile(userUpdateInfo);
+
+    var myServiceRoute = MaterialPageRoute(
+      builder: (BuildContext context) => MyService(),
+    );
+    Navigator.of(context)
+        .pushAndRemoveUntil(myServiceRoute, (Route<dynamic> route) => false);
+  }
+
+  void myAlert(String titleString, String messageString) {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: ListTile(
+              leading: Icon(
+                Icons.android,
+                size: 48.0,
+              ),
+              title: Text(titleString),
+            ),
+            content: Text(messageString),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('OK'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              )
+            ],
+          );
+        });
   }
 
   @override
@@ -84,14 +176,17 @@ class _SignUpState extends State<SignUp> {
           uploadButton(),
         ],
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          showHead(),
-          nameText(),
-          emailText(),
-          passwordText(),
-        ],
+      body: Form(
+        key: formKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            showHead(),
+            nameText(),
+            emailText(),
+            passwordText(),
+          ],
+        ),
       ),
     );
   }
